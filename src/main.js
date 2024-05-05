99f0a5eeee08ebf65fd40ea5ff7b94ae7173e67d
Nuckle <coil@itscat.org>
2024-05-05 23:18:17 +0300

    Added dark/light theme toggle button

--- src/main.js
+++ src/main.js
@@ -96,6 +96,42 @@ document.addEventListener('DOMContentLoaded', function () {
 		input.value += input.textContent || text;
 	});
 
+	const toggleColorMode = (e) => {
+		// Switch to Light Mode
+		if (e.currentTarget.classList.contains('light--hidden')) {
+			// Sets the custom HTML attribute
+			document.documentElement.setAttribute('color-mode', 'light');
+
+			//Sets the user's preference in local storage
+			localStorage.setItem('color-mode', 'light');
+			return;
+		}
+
+		/* Switch to Dark Mode
+        Sets the custom HTML attribute */
+		document.documentElement.setAttribute('color-mode', 'dark');
+
+		// Sets the user's preference in local storage
+		localStorage.setItem('color-mode', 'dark');
+	};
+
+	// Get the buttons in the DOM
+	const toggleColorButtons = document.querySelectorAll('.color-mode-btn');
+
+	// Set up event listeners
+	toggleColorButtons.forEach((btn) => {
+		btn.addEventListener('click', toggleColorMode);
+	});
+
+	if (
+		/* This condition checks whether the user has set a site preference for dark mode OR a OS-level preference for Dark Mode AND no site preference */
+		localStorage.getItem('color-mode') === 'dark' ||
+		(window.matchMedia('(prefers-color-scheme: dark)').matches && !localStorage.getItem('color-mode'))
+	) {
+		// if true, set the site to Dark Mode
+		document.documentElement.setAttribute('color-mode', 'dark');
+	}
+
 	registerSW({
 		immediate: true,
 	});
